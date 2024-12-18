2fe346f35bdb13d98f60f7168427f1edb6d6aa80
KornachevaOlga <167702088+KornachevaOlga@users.noreply.github.com>
2024-12-18 21:11:12 +0300

    module_19

--- task1/admin.py
+++ task1/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Buyer, Game
 
 # Register your models here.
+class BuyerAdmin(admin.ModelAdmin):
+    # Ограничение количества записей на странице
+    list_per_page = 20
+
+    # Поля, которые будут отображаться в списке
+    list_display = ('title', 'cost', 'size')
+
+    # Поля, по которым будет доступна фильтрация
+    list_filter = ('size', 'cost')
+
+    # Поле, по которому будет доступен поиск
+    search_fields = ('title',)
+
+
+# Регистрация модели Buyer с админ-классом BuyerAdmin
+admin.site.register(Game, BuyerAdmin)
+
+
+class GameAdmin(admin.ModelAdmin):
+    # Ограничение количества записей на странице
+    list_per_page = 30
+
+    # Поля, которые будут отображаться в списке
+    list_display = ('name', 'balance', 'age')
+
+    # Поля, по которым будет доступна фильтрация
+    list_filter = ('balance', 'age')
+
+    # Поле, по которому будет доступен поиск
+    search_fields = ('name',)
+
+    # Делаем поле balance доступным только для чтения
+    readonly_fields = ('balance',)
+
+
+# Регистрация модели Game с админ-классом GameAdmin
+admin.site.register(Game, GameAdmin)
+
+
